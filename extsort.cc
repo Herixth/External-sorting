f4358072f3294da3f2c8693a0c1316430c46eddb
Herixth <herixth@gmail.com>
2019-01-06 13:41:04 +0800

    herixth 2019-1-6

--- extsort.cc
+++ extsort.cc
@@ -5,12 +5,10 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
     Extsort_Manager extsort(argv);
-
     extsort.init();
     extsort.create_runfile_RS();
     extsort.merge();
 
-    
 
     //system("pause");
     return 0;
